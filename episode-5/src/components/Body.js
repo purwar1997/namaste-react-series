d1b146708dd21459f6973d91b2b44b54cca59379
purwar1997 <shubhampurwar511@gmail.com>
2023-07-24 02:02:56 +0530

    added swiggy API to fetch data

--- episode-5/src/components/Body.js
+++ episode-5/src/components/Body.js
@@ -1,9 +1,35 @@
-import { useState } from '../../node_modules/react';
+import { useState, useEffect } from '../../node_modules/react';
 import RestaurantCard from './RestaurantCard';
-import { restaurantList } from '../utils/mockData';
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://www.swiggy.com/mapi/homepage/getCards?lat=12.9715987&lng=77.5945627'
+      );
+
+      const json = await response.json();
+
+      const restaurantList =
+        json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants;
+
+      setRestaurants(restaurantList);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <div className='body'>
