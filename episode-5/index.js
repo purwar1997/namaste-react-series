c66262fb63de742758617d2fa5b3f99bdb1ee0b8
purwar1997 <shubhampurwar511@gmail.com>
2023-07-23 12:14:43 +0530

    project initialized in episode-5

---
+++ episode-5/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const AppLayout = () => {
+  return <h1>Hi</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
