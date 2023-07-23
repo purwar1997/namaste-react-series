b401e869716185a3d477544d8d8d8eb02e754018
purwar1997 <shubhampurwar511@gmail.com>
2023-07-23 21:28:35 +0530

    completed episode-5 of namaste react

--- episode-5/src/components/Body.js
+++ episode-5/src/components/Body.js
@@ -1,10 +1,13 @@
+import { useState } from '../../node_modules/react';
 import RestaurantCard from './RestaurantCard';
 import { restaurantList } from '../utils/mockData';
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
   return (
     <div className='body'>
-      <div className='search-container'>
+      {/* <div className='search-container'>
         <input
           className='search-input'
           type='search'
@@ -12,10 +15,21 @@ const Body = () => {
           placeholder='Search for food and restaurants'
         />
         <button className='search-btn'>Search</button>
+      </div> */}
+
+      <div className='filters'>
+        <button
+          className='top-rated'
+          onClick={() =>
+            setRestaurants(restaurants.filter(restaurant => restaurant?.info?.avgRating > 4.0))
+          }
+        >
+          Top rated
+        </button>
       </div>
 
       <div className='restaurant-list'>
-        {restaurantList.map(restaurant => (
+        {restaurants.map(restaurant => (
           <RestaurantCard key={restaurant?.info?.id} restaurantData={restaurant} />
         ))}
       </div>
