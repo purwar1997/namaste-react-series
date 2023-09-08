90e569caf63e68c0c311e81a1d440e1618010406
purwar1997 <shubhampurwar511@gmail.com>
2023-09-08 23:09:18 +0530

    added Cart component

--- episode-12/src/components/Cart.js
+++ episode-12/src/components/Cart.js
@@ -1,7 +1,14 @@
 import useFetchRestaurantData from '../utils/useFetchRestaurantData';
+import Shimmer from './Shimmer';
 
 const Cart = ({ cartItems }) => {
-  //  const restaurantInfo = useFetchRestaurantData();
+  const restaurantData = useFetchRestaurantData(cartItems[0].restaurantId);
+
+  if (restaurantData === null) {
+    return <Shimmer />;
+  }
+
+  const restaurantInfo = restaurantData?.cards[0]?.card?.card?.info;
 
   console.log(cartItems);
 
